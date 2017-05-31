3d381c500fc6c8efcde449222b8631746bf78d05
Arthur Matos <arthurm104@gmail.com>
2017-05-31 11:29:01 -0300

    Enhances control_singlecycle module
    - Adds jal_enable and jalr_enable signals;
    - Assigns pc_write_enable to control_signals' MSB;
    - Adds pretty comments to identify columns easily :sparkles:

--- core/RV64I/SingleCycle/control_singlecycle.v
+++ core/RV64I/SingleCycle/control_singlecycle.v
@@ -3,63 +3,79 @@
 module control_singlecycle (
     input  [6:0] inst_opcode,
 
+    output pc_write_enable,         // Habilita escrita de PC
+    output jal_enable,
+    output jalr_enable,
     output branch_enable,
     output data_mem_read_enable,    // NOTE: Read enable é necessário?
     output data_mem_write_enable,   // Habilita escrita na memória de dados
     output regfile_write_enable,    // Habilita escrita no regfile
-    output pc_write_enable,         // Habilita escrita de PC
     output [1:0] mem_to_reg_sel,    // Seleciona a entrada de escrita do regfile
     output [1:0] alu_op,            // Seleciona a funcionalidade da ALU
     output [1:0] alu_sel_src_a,     // Seleciona a entrada A da ALU
-    output [1:0] alu_sel_src_b      // Seleciona a entrada B da ALU
+    output alu_sel_src_b            // Seleciona a entrada B da ALU
 );
 
-reg  [12:0] control_signals;
+reg  [13:0] control_signals;
 
-assign branch_enable            = control_signals[12];
-assign data_mem_read_enable     = control_signals[11];
-assign data_mem_write_enable    = control_signals[10];
-assign regfile_write_enable     = control_signals[9];
-assign pc_write_enable          = control_signals[8];
-assign mem_to_reg_sel           = control_signals[7:6];
-assign alu_op                   = control_signals[5:4];
-assign alu_sel_src_a            = control_signals[3:2];
-assign alu_sel_src_b            = control_signals[1:0];
+assign pc_write_enable          = control_signals[13];
+assign jal_enable               = control_signals[12];
+assign jalr_enable              = control_signals[11];
+assign branch_enable            = control_signals[10];
+assign data_mem_read_enable     = control_signals[9];
+assign data_mem_write_enable    = control_signals[8];
+assign regfile_write_enable     = control_signals[7];
+assign mem_to_reg_sel           = control_signals[6:5];
+assign alu_op                   = control_signals[4:3];
+assign alu_sel_src_a            = control_signals[2:1];     // rs1 || pc || 64'b0 || XXXX
+assign alu_sel_src_b            = control_signals[0];
 
 
 // NOTE: A estratégia de código compacto é uma boa ideia?
 
-// TODO: passar pc_write_enable para o MSB (fica mais fácil de desativá-lo);
-
 initial begin
-    control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b00 , 2'b00 , 2'b00 };
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //                                                                                                        //
+            //                                             data_mem_write_enable                                      //
+            //                                                       |                                                //
+            //                                 data_mem_read_enable  |  regfile_write_enable                          //
+            //                                                |      |      |                                         //
+            //                                 branch_enable  |      |      |   mem_to_reg_sel                        //
+            //                                         |      |      |      |       |                                 //
+            //                           jalr_enable   |      |      |      |       |    alu_op                       //
+            //                                  |      |      |      |      |       |       |                         //
+            //                      jal_enable  |      |      |      |      |       |       |   alu_sel_src_a         //
+            //                           |      |      |      |      |      |       |       |       |                 //
+            //         pc_write_enable   |      |      |      |      |      |       |       |       |   alu_sel_src_b //
+            //                    |      |      |      |      |      |      |       |       |       |      |          //
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 2'b00 , 2'b00 , 2'b00 , 1'b0 };
 end
 
 always @ ( * ) begin
     case (inst_opcode)
         `OPC_LOAD:
-            control_signals = { 1'b0 , 1'b1 , 1'b0 , 1'b1 , 1'b1 , 2'b01 , 2'b00 , 2'b00 , 2'b01 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b0 , 1'b1 , 2'b01 , 2'b00 , 2'b00 , 1'b1 };
         // `OPC_LOAD_FP:
         // `OPC_custom0:
         // `OPC_MISC_MEM:
         `OPC_OP_IMM:
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 2'b01 };
-        `OPC_AUIPC:                                                                // PC
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b00 , 2'b01 , 2'b01 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 1'b1 };
+        `OPC_AUIPC:
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b00 , 2'b01 , 1'b1 };
         `OPC_OP_IMM_32:
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 2'b01 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 1'b1 };
         // `OPC_LEN_48b0:
         `OPC_STORE:
-            control_signals = { 1'b0 , 1'b0 , 1'b1 , 1'b0 , 1'b1 , 2'b00 , 2'b00 , 2'b00 , 2'b01 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b0 , 2'b00 , 2'b00 , 2'b00 , 1'b1 };
         // `OPC_STORE_FP:
         // `OPC_custom1:
         // `OPC_AMO:
         `OPC_OP:
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 2'b00 };
-        `OPC_LUI:                                                                 // 64'b0
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b00 , 2'b10 , 2'b01 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 1'b0 };
+        `OPC_LUI:
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b00 , 2'b10 , 1'b1 };
         `OPC_OP_32:
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 2'b00 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b10 , 2'b00 , 1'b0 };
         // `OPC_LEN_64b:
         // `OPC_MADD:
         // `OPC_MSUB:
@@ -70,19 +86,19 @@ always @ ( * ) begin
         // `OPC_custom2:
         // `OPC_LEN_48b1:
         `OPC_BRANCH:
-            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b11 , 2'b00 , 2'b00 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 2'b00 , 2'b11 , 2'b00 , 1'b0 };
         `OPC_JALR:
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b00 , 2'b00 , 2'b01 };
+            control_signals = { 1'b1 , 1'b0 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b00 , 2'b00 , 1'b1 };
         // `OPC_reserved1:
-        `OPC_JAL:                                                                  // PC
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b1 , 2'b00 , 2'b00 , 2'b01 , 2'b01 };
+        `OPC_JAL:
+            control_signals = { 1'b1 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b00 , 2'b01 , 1'b1 };
         // `OPC_SYSTEM:
         // `OPC_reserved2:
         // `OPC_custom3:
         // `OPC_LEN_80b:
 
         default:
-            control_signals = { 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 2'b00 , 2'b00 , 2'b00 , 2'b00 };
+            control_signals = { 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 2'b00 , 2'b00 , 2'b00 , 1'b0 };
 
     endcase
 end
